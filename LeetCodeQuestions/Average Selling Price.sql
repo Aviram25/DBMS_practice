e11450cbc4a15bebbc0c4351682093dc6e0bae80
Aviram Dhagat <dhagataviram@gmail.com>
2026-02-03 22:39:58 +0530

    Create Average Selling Price.sql

---
+++ LeetCodeQuestions/Average Selling Price.sql
@@ -0,0 +1,5 @@
+SELECT p.product_id ,ifnull( round(sum(p.price*us.units) / sum(us.units),2),0) as average_price
+from Prices p left join UnitsSold us
+on p.product_id = us.product_id
+AND us.purchase_date  between p.start_date and p.end_date
+group by p.product_id;
