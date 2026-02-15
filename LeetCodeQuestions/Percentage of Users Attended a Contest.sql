92159049ff3c492640283146823c3f775c165c3f
Aviram Dhagat <dhagataviram@gmail.com>
2026-02-15 14:47:14 +0530

    Add SQL query to calculate contest attendance percentage

---
+++ LeetCodeQuestions/Percentage of Users Attended a Contest.sql
@@ -0,0 +1,5 @@
+select contest_id, round((count(distinct(r.user_id))/(select count(distinct user_id) from Users)*100),2) as percentage
+from Users u join Register r
+on u.user_id = r.user_id
+group by contest_id
+Order by percentage desc, contest_id ;
