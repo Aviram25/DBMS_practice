48f391abd695e6b08fd562dc9da75e1afc397318
Aviram Dhagat <dhagataviram@gmail.com>
2026-02-21 16:05:44 +0530

    Create User Activity for the Past 30 Days.sql

---
+++ LeetCodeQuestions/User Activity for the Past 30 Days.sql
@@ -0,0 +1,4 @@
+SELECT activity_date as day, count(distinct(user_id)) as active_users
+FROM Activity
+WHERE activity_date >= '2019-06-28' and activity_date <= '2019-07-27' 
+GROUP BY activity_date; 
